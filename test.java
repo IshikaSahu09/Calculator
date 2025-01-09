49f4be33828b030ba8f493d6af46622a3f15e779
Ishika Sahu <149892484+IshikaSahu09@users.noreply.github.com>
2025-01-09 12:25:18 +0530

    Update test.java

--- test.java
+++ test.java
@@ -1,5 +1,15 @@
-int a = 10;
-int b= 20;
-int sum = a + b;
+import java.util.*;
+
+public class calc{
+public static void main(String[] args){
+
+Scanner sc = new Scanner(System.in);
+int a = sc.nextInt();
+int c= sc.nextInt();
+int sum = a+b;
 System.out.println("Addition is: " + sum);
+sc.close();
+}
+}
+
 
